c037e0a8728e329559838f9c6410abb75479d478
ruben compres <ruben compres>
2016-08-06 17:59:49 -0500

    made burger.js in models and completed controller.js

--- controller/burger_controller.js
+++ controller/burger_controller.js
@@ -1,37 +1,54 @@
-var express = require('express');
-var router = express.Router();
-var burger = require('../models/burger.js');
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
+const methodOverride = require('methodOverride');
+const bodyParser = require('body-parser');
+
+
 
 //get route -> index
 router.get('/', function(req,res) {
 		res.redirect('/burgers')
 });
-
 router.get('/burgers', function(req,res) {
-	//express callback response by calling burger.selectAllBurger
-	burger.all(function(burger_data){
-		//wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-		res.render('index', burger_data);
+	models.burgers.findAll().then(function(data){
+		res.render('index',{burgers: data});
 	});
 });
 
 //post route -> back to index
 router.post('/burgers/create', function(req, res) {
-	//takes the request object using it as input for buger.addBurger
-	burger.create(req.body.burger_name, function(result){
-		//wrapper for orm.js that using MySQL insert callback will return a log to console, render back to index with handle
-		console.log(result);
-		res.redirect('/');
+	models.burger.create({
+		burger_name:req.body.name,
+		devoured: 0
+	}).then(function(){
+		res.redirect('/burgers');
 	});
 });
 
 //put route -> back to index
-router.put('/burgers/update', function(req,res){
-	burger.update(req.body.burger_id, function(result){
-		//wrapper for orm.js that using MySQL update callback will return a log to console, render back to index with handle
-		console.log(result);
-		res.redirect('/');
+router.put('/burgers/update/devour/:id', function(req,res){
+	models.burger.update({
+		devoured:1
+	},{where:{
+		id:req.params.id
+	}}).then(function(){
+		res.redirect('/burgers');
 	});
+
 });
+
+router.delete('/burgers/delete/:id', function(req, res){
+	models.burgers.destroy(
+		{where:{
+			id:req.params.id
+		}}).then(function(){
+			res.redirect('/burgers');
+		})
+})
+
+router.use(function(req, res) {
+	res.redirect('/burgers');
+})
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,14 @@
-var orm = require('../config/orm.js');
-
-var burger = {
-	all: function(cb) {
-		orm.all('burgers', function(res){
-			cb(res)
-		});
-	},
-	create: function(name,cb) {
-		orm.create('burgers', ['burger_name', 'devoured'], [name, false], cb);
-	},
-	update: function(id, cb) {
-		var condition = 'id=' + id;
-		orm.update('burgers', {devoured : true}, condition, cb);
-	}
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var burgers = sequelize.define('burgers', {
+    burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return burgers;
 };
-
-module.exports = burger;
